f9af267272fad67ac196f6315b14fa02ed9818ff
SakuraWald <sakurawald@gmail.com>
2023-04-30 20:47:08 +0800

    save

---
+++ paint.sh
@@ -0,0 +1,4 @@
+touch $(date +"%Y-%m-%d")
+git add .
+git commit -m "save"
+git push
---
+++ painter.sh
@@ -0,0 +1,7 @@
+for i in {1..19477}; do 
+  j=$(date -d "1970-01-01 +$i days")
+  touch $j
+  git add .
+  git commit --date $j -m "save"
+done
+  git push
